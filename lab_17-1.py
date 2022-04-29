bb51006fac7cf6cb473e8748bf99c4a6e2edb8c9
p22mgiroux <p22mgiroux@fairfieldprep.org>
2022-04-28 22:09:33 -0400

    Finish Labs 17-(1-2)

---
+++ lab_17-1.py
@@ -0,0 +1,21 @@
+# Author: MOG 4/28/22
+
+import math
+
+class Circle:
+    """ Circle class represents a circle """
+    def __init__(self, rad):
+        """ Create a new circle with radius 1 """
+        self.radius = rad
+
+    def get_diameter(self):
+        """ Calculate diameter of circle """
+        return self.radius * 2
+
+    def get_area(self):
+        """ Calculate area of circle """
+        return self.radius ** 2 * math.pi
+
+my_circle = Circle(3)
+
+print(my_circle.get_area())
---
+++ lab_17-2.py
@@ -0,0 +1,25 @@
+# Author: MOG 4/28/22
+
+import math
+
+class Circle:
+    """ Circle class represents a circle """
+    def __init__(self, rad):
+        """ Create a new circle with radius 1 """
+        self.radius = rad
+
+    def get_diameter(self):
+        """ Calculate diameter of circle """
+        return self.radius * 2
+
+    def get_area(self):
+        """ Calculate area of circle """
+        return self.radius ** 2 * math.pi
+
+    def get_perimeter(self):
+        """ Calculate perimeter of circle """
+        return self.get_diameter() * math.pi
+
+my_circle = Circle(3)
+
+print(my_circle.get_perimeter())
